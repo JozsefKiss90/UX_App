89bd53b032d7f83d9212f6fda65987063bc5346f
silentstorm902 <jozsefkiss90@gmail.com>
2023-10-11 17:56:18 +0200

    removed likert images

--- pages/tasks/experience.tsx
+++ pages/tasks/experience.tsx
@@ -10,11 +10,6 @@ function Experience() {
           <p style={{ margin: 0 }}>
             Please indicate your level of experience<br />with Microsoft Excel
           </p>
-          <img 
-            src="/images/excel.png" 
-            alt="Microsoft Excel" 
-            style={{ width: '50px', height: '50px', marginLeft: '10px' }} 
-          />
         </div>
       ),
       ranking: ["Never used", <>I use it almost <br /> every day</>]
@@ -31,21 +26,18 @@ function Experience() {
     },
     2: {
       question: (
-        <div style={{ display: 'flex', flexDirection: "column", alignItems: 'center' }}>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
           <p style={{ margin: 0 }}>
-            On a scale of 1 to 5, please rate your proficiency level
+          On a scale of 1 to 5, please rate your proficiency level<br /> in spreadsheet apps
           </p>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <p>in spreadsheet apps&nbsp;</p>
-          </div>
         </div>
       ),
       ranking: ["Beginner", "Expert"]
     },
     3: {
       question: (
-        <div style={{ position: 'relative', textAlign: 'center' }}>
-          <p style={{ display: 'inline-block', margin: 0 }}>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <p style={{ margin: 0 }}>
             How would you describe your experience <br /> with the decimal increase / decrease button?
           </p>
         </div>
